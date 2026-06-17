988c96b97da8fd55c30c219e1afa7732a14af7c4
Atharva-Kavale <atharvakavale32@gmail.com>
2026-06-17 10:31:10 +0530

    update to new keka

--- contentScript.js
+++ contentScript.js
@@ -59,35 +59,126 @@
       return `${hh}h : ${mm}m`;
     }
 
-    const cardBody = document.getElementsByClassName("card-body");
+    function getByXpath(path) {
+      return document.evaluate(
+        path,
+        document,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        null,
+      ).singleNodeValue;
+    }
 
-    const latestLog = cardBody[cardBody.length - 1]
-      .getElementsByClassName("dropdown attendance-logs-row")[0]
-      .getElementsByClassName(
-        "d-flex align-items-center px-16 py-12 on-hover border-bottom"
-      )[0];
+    function paintToDOM(flagLog, fTime, cBody) {
+      if (!cBody) {
+        console.log("cardbody is not present");
+        return;
+      }
 
-    if (!document.getElementsByClassName("open")[1]) {
+      if (cBody.children.length > 1) {
+        cBody.removeChild(cBody.lastChild);
+      }
+
+      const finalTimerDisplayEl = getElement("div");
+
+      const div1 = getElement("div");
+      const div2 = getElement("div");
+      const div3 = getElement("div");
+
+      //Div1 container starts
+      const timerImg = getElement("img");
+      timerImg.src = chrome.runtime.getURL("assets/clock.svg");
+      timerImg.width = "30";
+
+      div1.appendChild(timerImg);
+      //Div1 container ends
+
+      //Div2 container starts
+      const timerSpan = getElement("span");
+      timerSpan.innerText = flagLog ? fTime : "User not logged yet";
+      timerSpan.style.fontWeight = "500";
+      timerSpan.style.fontSize = "18px";
+
+      div2.appendChild(timerSpan);
+      //Div2 container ends
+
+      //Div3 container starts
+      const refreshImg = getElement("img");
+      refreshImg.src = chrome.runtime.getURL("assets/refresh.png");
+      refreshImg.style.padding = "15px";
+      refreshImg.style.borderRadius = "50%";
+      refreshImg.style.transitionDuration = "2000ms";
+      refreshImg.style.transform = "rotate(0deg)";
+      refreshImg.style.opacity = "0";
+      refreshImg.style.cursor = "pointer";
+
+      setTimeout(() => {
+        refreshImg.style.transform = "rotate(720deg)";
+        refreshImg.style.opacity = "1";
+      }, 10);
+
+      refreshImg.addEventListener("click", () => {
+        cBody.removeChild(cBody.lastChild);
+        scriptRunner();
+      });
+
+      div3.appendChild(refreshImg);
+      //Div3 container ends
+
+      finalTimerDisplayEl.appendChild(div1);
+      finalTimerDisplayEl.appendChild(div2);
+      finalTimerDisplayEl.appendChild(div3);
+
+      finalTimerDisplayEl.style.flexGrow = 1;
+      finalTimerDisplayEl.style.display = "flex";
+      finalTimerDisplayEl.style.alignItems = "center";
+      finalTimerDisplayEl.style.gap = "5px";
+
+      cBody.appendChild(finalTimerDisplayEl);
+    }
+
+    const cardBodyXpath =
+      '//*[@id="preload"]/xhr-app-root/div/employee-me/div/employee-attendance/div/div/div/div/employee-attendance-stats/div/div[3]/employee-attendance-request-actions/div/div/div';
+    const cardBody = getByXpath(cardBodyXpath);
+
+    const lastLogXpath =
+      '//*[@id="preload"]/xhr-app-root/div/employee-me/div/employee-attendance/div/div/div/div/div/employee-attendance-logs/div/employee-attendance-list-view/div/div[2]/div[1]/div/div[1]/div/div[2]/div/div[6]/div/span';
+    const latestLog = getByXpath(lastLogXpath);
+
+    let isUserLogged = !!latestLog;
+
+    if (!isUserLogged) {
+      paintToDOM(isUserLogged, "", cardBody);
+      return;
+    }
+
+    if (!document.getElementsByClassName("open")[1] && isUserLogged) {
       latestLog.click();
     }
 
-    const logData = document
-      .getElementsByClassName("open")[1]
-      .querySelectorAll("span:not([class])");
+    const logDataXpath =
+      '//*[@id="preload"]/xhr-app-root/div/employee-me/div/employee-attendance/div/div/div/div/div/employee-attendance-logs/div/employee-attendance-list-view/div/div[2]/div[1]/div/div[2]/div/div[2]/div[2]/div/div[2]/div';
+
+    const logData = getByXpath(logDataXpath).querySelectorAll(
+      "span.ng-star-inserted",
+    );
+
     const lastClockInString = logData[logData.length - 2].innerText;
     const temp = lastClockInString.split(" ");
     const lastClockInArr = temp[0].split(":");
     lastClockInArr.push(temp[1]);
-    const lastEffectiveHourString =
-      latestLog.querySelectorAll("span:not([class])")[0].innerText;
+
+    const lastEffectiveHourString = document
+      .getElementsByClassName("open")[0]
+      .querySelectorAll("span:not([class])")[0].innerText;
     const lastEffectiveHourArr = [];
 
-    let finalTime;
+    let finalTime = 0;
 
     if (logData[logData.length - 1].innerText == "MISSING") {
       lastEffectiveHourArr.push(lastEffectiveHourString.split("h")[0]);
       lastEffectiveHourArr.push(
-        lastEffectiveHourString.split("m +")[0].split(" ").pop()
+        lastEffectiveHourString.split("m +")[0].split(" ").pop(),
       );
       const a1 = [...lastClockInArr];
       let effectiveHours = [...lastEffectiveHourArr];
@@ -98,68 +189,11 @@
       finalTime = lastEffectiveHourString;
     }
 
-    console.log(finalTime);
-
     function getElement(eleName) {
       return document.createElement(eleName);
     }
 
-    const finalTimerDisplayEl = getElement("div");
-
-    const div1 = getElement("div");
-    const div2 = getElement("div");
-    const div3 = getElement("div");
-
-    //Div1 container starts
-    const timerImg = getElement("img");
-    timerImg.src = chrome.runtime.getURL("assets/clock.svg");
-    timerImg.width = "30";
-
-    div1.appendChild(timerImg);
-    //Div1 container ends
-
-    //Div2 container starts
-    const timerSpan = getElement("span");
-    timerSpan.innerText = finalTime;
-    timerSpan.style.fontWeight = "500";
-    timerSpan.style.fontSize = "18px";
-
-    div2.appendChild(timerSpan);
-    //Div2 container ends
-
-    //Div3 container starts
-    const refreshImg = getElement("img");
-    refreshImg.src = chrome.runtime.getURL("assets/refresh.png");
-    refreshImg.style.padding = "15px";
-    refreshImg.style.borderRadius = "50%";
-    refreshImg.style.transitionDuration = "2000ms";
-    refreshImg.style.transform = "rotate(0deg)";
-    refreshImg.style.opacity = "0";
-    refreshImg.style.cursor = "pointer";
-
-    setTimeout(() => {
-      refreshImg.style.transform = "rotate(720deg)";
-      refreshImg.style.opacity = "1";
-    }, 10);
-
-    refreshImg.addEventListener("click", () => {
-      cardBody[2].removeChild(cardBody[2].lastChild);
-      scriptRunner();
-    });
-
-    div3.appendChild(refreshImg);
-    //Div3 container ends
-
-    finalTimerDisplayEl.appendChild(div1);
-    finalTimerDisplayEl.appendChild(div2);
-    finalTimerDisplayEl.appendChild(div3);
-
-    finalTimerDisplayEl.style.flexGrow = 1;
-    finalTimerDisplayEl.style.display = "flex";
-    finalTimerDisplayEl.style.alignItems = "center";
-    finalTimerDisplayEl.style.gap = "5px";
-
-    cardBody[2].appendChild(finalTimerDisplayEl);
+    paintToDOM(isUserLogged, finalTime, cardBody);
 
     latestLog.click();
   }
@@ -168,12 +202,25 @@
     try {
       scriptRunner();
     } catch (error) {
-      console.warn("DOM is not ready yet");
-      console.info("Retrying...");
-
-      setTimeout(Executer, 1000);
+      console.warn("Unkown error to DOM", count);
+      console.log("Retrying...");
+      count++;
+      if (count < 10) setTimeout(Executer, 1000);
     }
   }
 
-  setTimeout(Executer, 1000);
+  let count = 0,
+    flag = -1;
+
+  // Listen for dynamic URL changes
+  navigation.addEventListener("navigatesuccess", () => {
+    handleUrlChange(window.location.href);
+  });
+
+  function handleUrlChange(currentUrl) {
+    if (currentUrl.slice(-20) == "#/me/attendance/logs") {
+      console.log("URL matched, starting script...");
+      setTimeout(Executer, 1000);
+    }
+  }
 })();
--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("button").onclick = () => {
-  chrome.cookies.getAll({ domain: ".keka.com" }, (res) => {
-    document.getElementById("text-field").innerText = JSON.stringify(res[1]);
-  });
-};
